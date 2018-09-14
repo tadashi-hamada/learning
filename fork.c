35ed9e1abe6b81c0928e6d9a58d4a4c15b7484cc
Lazer <35189816+tadashi-hamada@users.noreply.github.com>
2018-09-15 00:05:18 +0530

    Update fork.c

--- fork.c
+++ fork.c
@@ -1,5 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include  <sys/types.h>
+#include  <sys/wait.h>
+#include <unistd.h>
 void main()
 {
   int p1,p2,p3,p4,p5;
@@ -10,60 +13,66 @@ void main()
 	p2=fork();
 	if(p2==0)
 	  {//c3
-		printf("child c3 starts\n");
+		printf("\nchild c3 starts\n");
 		c3();
 		
 	}
 	else{//c1
-		printf("parent c1 starts\n");	
+		printf("\nparent c1 starts\n");	
 		c1();
 	 }
    }  
    else
     {//p
-	printf("parent p1 starts\n");	
-    p();
+
+
         p3=fork();
 		if(p3==0)//c2
 		  {
-                	printf("child c2 starts\n");
+
+                	//printf("child c2 starts\n");
 			p4=fork();
-		      if(p4==0)//c2
+		      if(p4==0)//c4
 		       {
-                	  printf("child c2 starts\n");
-			   c2();
+
+                	  printf("\nchild c4 starts\n");
+			   c4();
 		        }
-		     else{//c4
-			 printf("parent c4 starts\n");
-			 c4();
+		     else{//c2
+
+			 printf("\nparent c2 starts\n");
+			 c2();
 			}
 		}
-  
+   if(p1!=0&&p2!=0&&p3!=0){
+  	printf("\nparent p1 starts\n");	
+        p();}
     }
 }
+
 void p()
 {
-  for(int i=100;i<300;i++)
-	printf("pc1--%d pid %d\n",i,getpid());
+  for(int i=1200;i<3000;i++)
+	printf("\t%d           pid --%d\n",i,getpid());	
 }
 void c1()
 {
-  for(int i=20;i<40;i++)
-	printf("pc1--%d pid %d\n",i,getpid());
+  for(int i=200;i<600;i++)
+	printf("\t%d           pid --%d\n",i,getpid());	
 }
 void c2()
 {
-  for(int i=40;i<60;i++)
-	printf("pc1--%d pid %d\n",i,getpid());
+  for(int i=600;i<1000;i++)
+	printf("\t%d          pid --%d\n",i,getpid());	
 }
 void c3()
 {
-  for(int i=60;i<80;i++)
-	printf("pc1--%d pid %d\n",i,getpid());
+  for(int i=1;i<200;i++)
+	printf("\t%d           pid --%d\n",i,getpid());	
 }
 void c4()
 {
-  for(int i=80;i<100;i++)
-	printf("pc1--%d pid %d\n",i,getpid());
+  for(int i=1000;i<1200;i++)
+	printf("\t%d            pid --%d\n",i,getpid());	
 }
 
